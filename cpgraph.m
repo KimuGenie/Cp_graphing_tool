7560b9d9bed3106dadeecb7e50fda0e074c5647f
KimuGenie <baby11686@naver.com>
2021-05-19 16:45:29 +0900

    colormap
    이제 그룹이 아무리 많아도 color가 부족하지 않아요

--- cpgraph.m
+++ cpgraph.m
@@ -7,7 +7,7 @@ coordinate=1;
 % 1. S-beta  2.x-y
 singlerotor=1;
 %single rotor의 Cp를 같이 표시하려면 1, 아니면 0
-grouping=1;
+grouping=0;
 %소수 몇 째 자리까지 반올림해서 같으면 같은 그룹으로 볼 것인지
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -15,8 +15,8 @@ grouping=1;
 load('cpdata.mat');
 cpdata=sortrows(cpdata);
 
-colormap = ["r", "b", "g", "c", "m", "y", "k", "[0.7 0.2 0.3]", "[0.2 0.5 0.7]", "[1 0.5 0]", "[0.5 0 1]"];
-
+colormap = parula;
+c = size(colormap, 1);
 
 if coordinate==1
 
@@ -26,16 +26,17 @@ data=cpdata(cpdata(:, 3) == n, :);
 
 u=unique(data(:, 1));
 uc=unique(cpdata(:, 1));
+cn=ceil(c/numel(uc))-1;
 
 figure(n)
 hold on
 for i=1:numel(u)
-plot(data(data(:, 1) == u(i), 2), data(data(:, 1) == u(i), 4), 'o-', 'color', colormap(uc == u(i)), 'markerfacecolor', colormap(uc == u(i)))
+plot(data(data(:, 1) == u(i), 2), data(data(:, 1) == u(i), 4), 'o-', 'color', colormap(find(uc == u(i), 1)*cn, :), 'markerfacecolor', colormap(find(uc == u(i), 1)*cn, :))
 end
 if singlerotor
     plot([min(cpdata(:, 2))-5 max(cpdata(:, 2))+5], [0.3451 0.3451], 'r--', 'linewidth', 1)
 end
-legend([strcat('S=', string(u),'D');"Single rotor"], 'location', 'best');
+legend([strcat('S=', string(u),'D');"Single rotor"], 'location', 'best', 'fontsize', 7);
 xlabel('β')
 ylabel('Cp')
 title(sprintf('Rotor %d', n))
@@ -51,22 +52,23 @@ data=cpdata(cpdata(:, 3) == n, :);
 
 u=unique(data(:, 2));
 uc=unique(cpdata(:, 2));
+cn=ceil(c/numel(uc))-1;
 
 figure(n+3)
 hold on
 for i=1:numel(u)
-plot(data(data(:, 2) == u(i), 1), data(data(:, 2) == u(i), 4), 'o-', 'color', colormap(uc == u(i)), 'markerfacecolor', colormap(uc == u(i)))
+plot(data(data(:, 2) == u(i), 1), data(data(:, 2) == u(i), 4), 'o-', 'color', colormap(find(uc == u(i), 1)*cn, :), 'markerfacecolor', colormap(find(uc == u(i), 1)*cn, :))
 end
 if singlerotor
-    plot([min(cpdata(:, 1))-1 max(cpdata(:, 1))+1], [0.3451 0.3451], 'r--', 'linewidth', 1)
+    plot([min(cpdata(:, 1))-0.5 max(cpdata(:, 1))+0.5], [0.3451 0.3451], 'r--', 'linewidth', 1)
 end
-legend([strcat('β=', string(u)); "Single rotor"], 'location', 'best');
+legend([strcat('β=', string(u)); "Single rotor"], 'location', 'best', 'fontsize', 7);
 xlabel('S(D)')
 ylabel('Cp')
 title(sprintf('Rotor %d', n))
 grid on
 set(gcf, 'position', [430*(n-1)+100 50 400 300])
-axis([min(cpdata(:, 1))-1 max(cpdata(:, 1))+1 min(cpdata(:, 4))-0.01 max(cpdata(:, 4))+0.01])
+axis([min(cpdata(:, 1))-0.5 max(cpdata(:, 1))+0.5 min(cpdata(:, 4))-0.01 max(cpdata(:, 4))+0.01])
 
 end
 
@@ -84,16 +86,17 @@ data=cpdata(cpdata(:, 3) == n, :);
 
 u=unique(round(data(:, 1), grouping));
 uc=unique(round(cpdata(:, 1), grouping));
+cn=ceil(c/numel(uc))-1;
 
 figure(n)
 hold on
 for i=1:numel(u)
-plot(data(round(data(:, 1), grouping) == u(i), 2), data(round(data(:, 1), grouping) == u(i), 4), 'o-', 'color', colormap(uc == u(i)), 'markerfacecolor', colormap(uc == u(i)))
+plot(data(round(data(:, 1), grouping) == u(i), 2), data(round(data(:, 1), grouping) == u(i), 4), 'o-', 'color', colormap(find(uc == u(i), 1)*cn, :), 'markerfacecolor', colormap(find(uc == u(i), 1)*cn, :))
 end
 if singlerotor
     plot([min(cpdata(:, 2))-1 max(cpdata(:, 2))+1], [0.3451 0.3451], 'r--', 'linewidth', 1)
 end
-legend([strcat('x=', string(u), 'D');"Single rotor"], 'location', 'best');
+legend([strcat('x=', string(u), 'D');"Single rotor"], 'location', 'best', 'fontsize', 7);
 xlabel('y(D)')
 ylabel('Cp')
 title(sprintf('Rotor %d', n))
@@ -109,16 +112,17 @@ data=cpdata(cpdata(:, 3) == n, :);
 
 u=unique(round(data(:, 2), grouping));
 uc=unique(round(cpdata(:, 2), grouping));
+cn=ceil(c/numel(uc))-1;
 
 figure(n+3)
 hold on
 for i=1:numel(u)
-plot(data(round(data(:, 2), grouping) == u(i), 1), data(round(data(:, 2), grouping) == u(i), 4), 'o-', 'color', colormap(uc == u(i)), 'markerfacecolor', colormap(uc == u(i)))
+plot(data(round(data(:, 2), grouping) == u(i), 1), data(round(data(:, 2), grouping) == u(i), 4), 'o-', 'color', colormap(find(uc == u(i), 1)*cn, :), 'markerfacecolor', colormap(find(uc == u(i), 1)*cn, :))
 end
 if singlerotor
     plot([min(cpdata(:, 1))-1 max(cpdata(:, 1))+1], [0.3451 0.3451], 'r--', 'linewidth', 1)
 end
-legend([strcat('y=', string(u), 'D');"Single rotor"], 'location', 'best');
+legend([strcat('y=', string(u), 'D');"Single rotor"], 'location', 'best', 'fontsize', 7);
 xlabel('x(D)')
 ylabel('Cp')
 title(sprintf('Rotor %d', n))
